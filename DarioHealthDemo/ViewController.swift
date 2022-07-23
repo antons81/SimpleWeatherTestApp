c77ddd609fef6149297cf92703f73ce9ebcc984f
Anton Stremovskiy <perlik@gmail.com>
2022-07-23 23:58:08 +0300

    Initial Commit

---
+++ DarioHealthDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DarioHealthDemo
+//
+//  Created by Anton Stremovskiy on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
